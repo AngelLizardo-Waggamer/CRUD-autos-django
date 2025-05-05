b94d928cc77101b517b53447201164ae51f0e127
Ángel Lizardo <aahlizardo.britanico@gmail.com>
2025-05-05 08:24:32 -0600

    Agregar templates y métodos http para el CRUD
    Se añadieron templates nuevos junto con sus respectivos métodos para realizar los métodos CRUD con los automóviles.
    Además, se creó un modelo de automóvil para poder almacenar en la base de datos los automóviles que se vayan creando.

--- admin_automoviles/app_admin_autos/models.py
+++ admin_automoviles/app_admin_autos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Automovil(models.Model):
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100)
+    anio = models.PositiveIntegerField()
+    color = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.marca} {self.modelo} ({self.anio})"
--- admin_automoviles/app_admin_autos/views.py
+++ admin_automoviles/app_admin_autos/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Automovil
 
-# Create your views here.
+class AutomovilListView(ListView):
+    model = Automovil
+    template_name ='automoviles_list.html'
+    context_object_name = 'automoviles'
+
+class AutomovilDetailView(DetailView):
+    model = Automovil
+    template_name = 'automovil_detail.html'
+    context_object_name = 'automovil'
+
+class AutomovilCreateView(CreateView):
+    model = Automovil
+    fields = ['marca', 'modelo', 'anio', 'color', 'precio']  # ajusta a tus campos
+    template_name = 'automovil_form.html'
+    success_url = reverse_lazy('automoviles:listar')
+
+class AutomovilUpdateView(UpdateView):
+    model = Automovil
+    fields = ['marca', 'modelo', 'anio', 'color', 'precio']
+    template_name = 'automovil_form.html'
+    success_url = reverse_lazy('automoviles:listar')
+
+class AutomovilDeleteView(DeleteView):
+    model = Automovil
+    template_name = 'automovil_confirm_delete.html'
+    success_url = reverse_lazy('automoviles:listar')
